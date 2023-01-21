b3d6794fcc4294fda86821288c1618d4808d9ece
Pacomelebris <pacomelebris@gmail.com>
2023-01-21 12:31:10 +0000

    fix: problem on usernames fixed and game function added but need a fix also(bug on user_number taking the value of Theo instead of an user input

--- number_guess.sh
+++ number_guess.sh
@@ -12,6 +12,8 @@ function main {
   NB_USERS=$($PSQL "SELECT COUNT(*) FROM users")
 
   COUNT=0
+  COUNT_USERNAME=$($PSQL "SELECT COUNT(*) FROM users")
+  
   echo "$USERNAMES" | while read EXISTING_USERNAME
   do 
     #count the number of users checked
@@ -19,20 +21,68 @@ function main {
     if [[ $USERNAME == $EXISTING_USERNAME ]]
     then
       #existing user
-      #get user data 
+      #get user data
       USER_GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE username = '$USERNAME'")
       USER_BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE username = '$USERNAME'") 
       #message welcome again
-      echo "Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses."
+      echo "Welcome back, $USERNAME! You have played $USER_GAMES_PLAYED games, and your best game took $USER_BEST_GAME guesses."
+      game
       
     
-    else
+    elif [[ $COUNT -eq $COUNT_USERNAME ]]
+      then
       #new user
       #message welcome
       echo "Welcome, $USERNAME! It looks like this is your first time here."
       ADD_USER_TO_USERS=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+      #game
+
     fi
   done 
 }
+
+
+
+function game {
+
+  #create a secret random number 
+  SECRET_NUMBER=$((1 + $RANDOM % 1000))
+
+  #get the user number guess
+  echo "Guess the secret number between 1 and 1000:"
+  
+  unset USER_NUMBER
+  read USER_NUMBER 
+
+  while [[ ! $USER_NUMBER =~ ^[1-9][0-9]{0,2}$ ]]
+  do
+    echo "That is not an integer, guess again:"
+    read USER_NUMBER
+  done
+
+  NUMBER_OF_GUESSES=1
+
+  while [ $USER_NUMBER -ne $SECRET_NUMBER ];
+  do
+    if [ $USER_NUMBER -gt $SECRET_NUMBER ];
+    then
+      echo "It's lower than that, guess again:"
+      read USER_NUMBER
+
+    
+    elif [ $USER_NUMBER -lt $SECRET_NUMBER ];
+    then
+      echo "It's higher than that, guess again:"
+      read USER_NUMBER
+
+    fi
+    ((NUMBER_OF_GUESSES++))
+  done
+
+  echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+  ADD_GAME=$($PSQL "INSERT INTO games(username, number_of_guesses) VALUES ('$USERNAME', '$NUMBER_OF_GUESSES')")
+ 
+}
+
 
 main 
