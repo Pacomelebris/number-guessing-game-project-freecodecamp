eaf99af5793a5135ba61a19875a43e5d6433d7e5
Pacomelebris <pacomelebris@gmail.com>
2023-01-22 03:55:58 +0000

    fix: game call from main bug + finished the game function

--- number_guess.sh
+++ number_guess.sh
@@ -5,7 +5,7 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 function main {
   #get the username 
   echo "Enter your username:"
-  read USERNAME
+  read INPUT_USERNAME
 
   #get all existing usernames 
   USERNAMES=$($PSQL "SELECT username FROM users")
@@ -18,70 +18,99 @@ function main {
   do 
     #count the number of users checked
     ((COUNT++))
-    if [[ $USERNAME == $EXISTING_USERNAME ]]
+    if [[ $INPUT_USERNAME == $EXISTING_USERNAME ]]
     then
       #existing user
       #get user data
-      USER_GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE username = '$USERNAME'")
-      USER_BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE username = '$USERNAME'") 
+      USER_GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE username = '$INPUT_USERNAME'")
+      USER_BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE username = '$INPUT_USERNAME'") 
       #message welcome again
-      echo "Welcome back, $USERNAME! You have played $USER_GAMES_PLAYED games, and your best game took $USER_BEST_GAME guesses."
-      game
+      echo "Welcome back, $INPUT_USERNAME! You have played $USER_GAMES_PLAYED games, and your best game took $USER_BEST_GAME guesses."
+      break
       
     
     elif [[ $COUNT -eq $COUNT_USERNAME ]]
       then
       #new user
       #message welcome
-      echo "Welcome, $USERNAME! It looks like this is your first time here."
-      ADD_USER_TO_USERS=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
-      #game
+      echo "Welcome, $INPUT_USERNAME! It looks like this is your first time here."
+      ADD_USER_TO_USERS=$($PSQL "INSERT INTO users(username) VALUES('$INPUT_USERNAME')")
+      break
 
     fi
-  done 
+    
+  done
+  game $INPUT_USERNAME
 }
 
 
 
 function game {
 
+  #get the username of the pplayer from the main function
+  INPUT_USERNAME=$1
+  echo "username = $INPUT_USERNAME"
+  
   #create a secret random number 
   SECRET_NUMBER=$((1 + $RANDOM % 1000))
+  echo "secret number = $SECRET_NUMBER"
 
   #get the user number guess
-  echo "Guess the secret number between 1 and 1000:"
-  
   unset USER_NUMBER
+  echo "Guess the secret number between 1 and 1000:"
   read USER_NUMBER 
 
+  #if not an int between 1 and 1000
   while [[ ! $USER_NUMBER =~ ^[1-9][0-9]{0,2}$ ]]
   do
     echo "That is not an integer, guess again:"
     read USER_NUMBER
   done
 
+  #while the secret number is not found
+  #initialize the count of guesses
   NUMBER_OF_GUESSES=1
-
   while [ $USER_NUMBER -ne $SECRET_NUMBER ];
   do
+
+    #if the input is higher
     if [ $USER_NUMBER -gt $SECRET_NUMBER ];
     then
       echo "It's lower than that, guess again:"
       read USER_NUMBER
+      
+      #if not an int between 1 and 1000
+      while [[ ! $USER_NUMBER =~ ^[1-9][0-9]{0,2}$ ]]
+      do
+        echo "That is not an integer, guess again:"
+        read USER_NUMBER
+      done
 
     
+    #if the input is lower
     elif [ $USER_NUMBER -lt $SECRET_NUMBER ];
     then
       echo "It's higher than that, guess again:"
       read USER_NUMBER
+      
+      #if not an int between 1 and 1000
+      while [[ ! $USER_NUMBER =~ ^[1-9][0-9]{0,2}$ ]]
+      do
+        echo "That is not an integer, guess again:"
+        read USER_NUMBER
+      done
 
     fi
+
+    #count the number of guesses
     ((NUMBER_OF_GUESSES++))
   done
 
+  #print the recap of the game
   echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
-  ADD_GAME=$($PSQL "INSERT INTO games(username, number_of_guesses) VALUES ('$USERNAME', '$NUMBER_OF_GUESSES')")
- 
+
+  #add the game data to the table games
+  ADD_GAME=$($PSQL "INSERT INTO games(username, number_of_guesses) VALUES ('$INPUT_USERNAME', '$NUMBER_OF_GUESSES')")
 }
 
 
