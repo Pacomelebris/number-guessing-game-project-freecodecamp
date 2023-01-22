822f2bcf1e56af0bfe3d5fff7218d595b9976552
Pacomelebris <pacomelebris@gmail.com>
2023-01-22 04:24:35 +0000

    fix: bug user first time without users in db

--- number_guess.sh
+++ number_guess.sh
@@ -11,19 +11,21 @@ function main {
   USERNAMES=$($PSQL "SELECT username FROM users")
   NB_USERS=$($PSQL "SELECT COUNT(*) FROM users")
 
+  #check if the user exist already or not
+  #initialize the count of users
   COUNT=0
   COUNT_USERNAME=$($PSQL "SELECT COUNT(*) FROM users")
+  echo "$COUNT_USERNAME"
   
   echo "$USERNAMES" | while read EXISTING_USERNAME
   do 
-    #count the number of users checked
-    ((COUNT++))
+
     if [[ $INPUT_USERNAME == $EXISTING_USERNAME ]]
     then
       #existing user
       #get user data
-      USER_GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE username = '$INPUT_USERNAME'")
-      USER_BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE username = '$INPUT_USERNAME'") 
+      USER_GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games WHERE username = '$INPUT_USERNAME'")
+      USER_BEST_GAME=$($PSQL "SELECT MIN(number_of_guesses) FROM games WHERE username = '$INPUT_USERNAME'") 
       #message welcome again
       echo "Welcome back, $INPUT_USERNAME! You have played $USER_GAMES_PLAYED games, and your best game took $USER_BEST_GAME guesses."
       break
@@ -38,7 +40,8 @@ function main {
       break
 
     fi
-    
+    #count the number of users checked
+    ((COUNT++))
   done
   game $INPUT_USERNAME
 }
@@ -49,11 +52,9 @@ function game {
 
   #get the username of the pplayer from the main function
   INPUT_USERNAME=$1
-  echo "username = $INPUT_USERNAME"
   
   #create a secret random number 
   SECRET_NUMBER=$((1 + $RANDOM % 1000))
-  echo "secret number = $SECRET_NUMBER"
 
   #get the user number guess
   unset USER_NUMBER
